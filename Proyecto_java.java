f64102239b53ec9b72808277e4c0e2ecefaa8ddc
Axel <axelcarrion229@gmail.com>
2022-03-10 21:42:50 -0600

    Estos son mis dos primeros proyectos en java.

---
+++ Proyecto_java.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_java;
+
+/**
+ *
+ * @author jea
+ */
+public class Proyecto_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esto no es un hola mundo");
+        int num1 = 2;
+        int num2 = 3;
+        int answer = num1 * num2;
+        System.out.println(answer);
+    }
+    
+}
